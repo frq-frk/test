f8ac500ff851565a173d7c2ee222b0a78767f5fc
Farooq Mohammad Mulla <59905701+frq-frk@users.noreply.github.com>
2025-07-19 22:12:35 +0530

    Update HealthCheckController.java

--- HealthCheckController.java
+++ HealthCheckController.java
@@ -22,5 +22,6 @@ public class HealthCheckController {
     }
     public void helperMethod(){
         System.out.pritln("It's all about just testing again now");
+        System.out.pritln("Adding new line for testing again");
     }
 }
